59f0e2fb78a684538be4fb8fb6ec46558fa79c84
dr-guangtou <obsglobe@outlook.com>
2019-01-18 11:56:25 -0800

    Change the header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-content>
+      <v-toolbar style="margin-bottom: 32px;">
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Observatories over the Globe</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat href="https://github.com/obsglobe">
+            <v-avatar size=42>
+              <img src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png">
+            </v-avatar>
+            Source Code</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
   </div>
 </template>
 
